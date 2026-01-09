cc71b283536ca1b20cbf6702a04fb757aafca351
Marko Djukic <marco.djukic@icloud.com>
2026-01-09 14:37:58 +0100

    cleaned up version

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-
 let unitinput = document.getElementById("unit-inpt")
 let convertBtn = document.getElementById("convert-btn")
 let lengthEl = document.getElementById("length-el")
